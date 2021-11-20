ca48374c2c9bcfc23d7909f9151bf14dc9cb46f7
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-11-20 09:29:26 -0600

    LazyLoad y Suspense

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,49 +1,44 @@
+import { Suspense } from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    NavLink,
-    Redirect,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
 } from 'react-router-dom';
 
 import logo from '../logo.svg';
 import { routes } from './routes';
 
 export const Navigation = () => {
-    return (
-        <Router>
-            <div className="main-layout">
-                <nav>
-                    <img src={logo} alt="React Logo" />
-                    <ul>
-                        {routes.map(({ path, name }) => (
-                            <li key={path}>
-                                <NavLink
-                                    to={path}
-                                    activeClassName="nav-active"
-                                    exact
-                                >
-                                    {name}
-                                </NavLink>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
+  return (
+    <Suspense fallback={<span>Loading...</span>}>
+      <Router>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              {routes.map(({ path, name }) => (
+                <li key={path}>
+                  <NavLink to={path} activeClassName="nav-active" exact>
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-                {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-                <Switch>
-                    {routes.map(({ path, Component }) => (
-                        <Route
-                            path={path}
-                            key={path}
-                            render={() => <Component />}
-                        />
-                    ))}
+          <Switch>
+            {routes.map(({ path, Component }) => (
+              <Route path={path} key={path} render={() => <Component />} />
+            ))}
 
-                    <Redirect to={routes[0].path} />
-                </Switch>
-            </div>
-        </Router>
-    );
+            <Redirect to={routes[0].path} />
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
+  );
 };
